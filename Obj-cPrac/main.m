a3864cb07b35fea54ee563c4a1b8218f94984141
Derek Kadwell <derek@dmajorstudios.com>
2013-12-06 20:36:15 -0500

    Fixed Obj-c/main.m

--- Obj-cPrac/main.m
+++ Obj-cPrac/main.m
@@ -1,7 +1,11 @@
+/* use
+* "clang -fobjc-arc -framework Foundation main.m -o prog1"
+* to make executable
+*/
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
-    @utoreleasepool{
+    @autoreleasepool{
         NSLog(@"Programng is fun!");
     }
 
