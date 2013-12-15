dbab3bfe0d10d9f6db6ef1a6fe1bfb123a16ef28
Derek Kadwell <derek@dmajorstudios.com>
2013-12-15 11:06:38 -0500

    made/modified my WordAdder program

--- ChineseReview/main.cpp
+++ ChineseReview/main.cpp
@@ -2,11 +2,14 @@
 #include <string>
 #include <vector>
 
+void tabCout(std::string);
+
 class Vocabulary {
 public:
     void add();//setter
     void display();//getter
     void remove();
+    void help();
 private:
     std::vector<std::string> words;
     std::vector<std::string>::iterator i;
@@ -33,24 +36,36 @@ private:
         }
     }
 
+    void Vocabulary::help() {
+        tabCout("touch -prompts you for a new word／phrase and then saves it");
+        tabCout("ls -list all the words");
+        tabCout("rm -prompts you for a word to PERMANENTLY delete");
+        tabCout("exit -exits the application");
+    }
+
 int main() {
+    std::cout << "type -help- for commands" << std::endl;
     Vocabulary vocab;
     bool running = true;
     std::string action;
     while (running) {
         std::cin >> action;
-        if(action == "add") {
+        if(action == "help") {
+            vocab.help();
+        } else if(action == "touch") {
             vocab.add();
-        } else if(action == "display") {
+        } else if(action == "ls") {
             vocab.display();
-        } else if(action == "remove") {
+        } else if(action == "rm") {
             vocab.remove();
         } else if(action == "exit") {
             std::cout << "Exited successfully." << std::endl;
             running = false;
-        } else {
-            std::cout << std::endl;
         }
     }
 return 0;
+}
+
+void tabCout(std::string string) {
+    std::cout << '\t' << string << std::endl;
 }
