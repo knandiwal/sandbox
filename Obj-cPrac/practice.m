b3236935eea758e7c697e1cd279ef9f8eda85535
Derek Kadwell <derek@dmajorstudios.com>
2013-12-15 16:34:21 -0500

    Made a file to play with examples in

---
+++ Obj-cPrac/practice.m
@@ -0,0 +1,16 @@
+// Find the greatest common divisor of two nonnegative integers
+#import <Foundation/Foundation.h>
+
+int main (int argc, char * argv[]) {
+    @autoreleasepool {
+        int number, right_digit;
+        NSLog (@"Enter your number.");
+        scanf ("%i", &number);
+        while ( number != 0 ) {
+            right_digit = number % 10;
+            NSLog (@"%i", right_digit);
+            number /= 10;
+        }
+    }
+return 0;
+}
