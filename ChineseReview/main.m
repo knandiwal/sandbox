87a91fe827cab29238daac95e47c5188593d9b03
Derek Kadwell <derek@dmajorstudios.com>
2013-12-17 13:14:25 -0500

    Started converting my chinese review app to Objective-c

--- ChineseReview/main.m
+++ ChineseReview/main.m
@@ -1,9 +1,17 @@
-#import <Foundation/Foundation.h>
+#import <Foundation/Foudation.h>
 
+@interface Quick: NSObject
+-(void) tabCout: (NSString) string;
+@end
 
+@interface Vocabulary: NSObject
+-(void) add;
+-(void) display;
+-(void) remove;
+-(void) help;
+@end
 
-int main(int argc, char const *argv[])
-{
-    /* code */
-    return 0;
+int main (int argc, const char *argv[]) {
+
+return 0;
 }
