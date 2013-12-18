ac54a35115aef21726a198bc1564b158e24c571c
Derek Kadwell <derek@dmajorstudios.com>
2013-12-18 12:18:29 -0500

    chapter6 exercise1

--- Obj-cPrac/exercise.m
+++ Obj-cPrac/exercise.m
@@ -2,14 +2,25 @@
 
 int main(int argc, char const *argv[]) {
     @autoreleasepool {
-        int fahrenheit = 27;
-        int celsius = 0;
-        //convertion f - c
-        celsius = (fahrenheit - 32) / 1.8;
+        // Declare two integers that will be tested to see
+        // if the first integer is evenly divisible by the second
+        int number1, number2;
+        bool evenlyDivisible;
 
-        NSLog (@"%i degrees Fahrenheit is equivalent to %i degrees Celsius", fahrenheit, celsius);
+        // Save the user inout into the variables
+        NSLog (@"Pleas input 2 numbers");
+        scanf ("%i", &number1);
+        scanf ("%i", &number2);
+
+        // Test to see whether number1 is evenly divisible by number2
+        (number1 % number2) == 0 ? (evenlyDivisible = true) : (evenlyDivisible = false);
+
+        // Display the appropriate message
+        if(evenlyDivisible == true) {
+            NSLog (@"The first number is evenly divisible by the second");
+        } else{
+            NSLog (@"The first number isn't evenly divisible by the second");
+        }
     }
-
-
-    return 0;
+return 0;
 }
