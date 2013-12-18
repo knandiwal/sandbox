a250836abcf5f52d3ddd80fd7cd615842d5503cb
Derek Kadwell <derek@dmajorstudios.com>
2013-12-18 13:50:26 -0500

    Seperating implementation and interface into their own files

---
+++ Obj-cPrac/Fraction.h
@@ -0,0 +1,11 @@
+// Created by Steve Kochan on 9/29/11.
+// Copyright (c) ClassroomM, Inc. All rights reserved. //
+#import <Foundation/Foundation.h> // The Fraction class
+@interface Fraction : NSObject
+-(void) print;
+-(void) setNumerator: (int) n;
+-(void) setDenominator: (int) d;
+-(int) numerator;
+-(int) denominator;
+-(double) convertToNum;
+@end
--- Obj-cPrac/main.m
+++ Obj-cPrac/main.m
@@ -1,58 +1,18 @@
-/* use
-* "clang -fobjc-arc -framework Foundation main.m -o prog1"
+/*
+* use
+* "clang -fobjc-arc -framework Foundation <.m files> -o prog1"
 * to make executable
 */
-
-#import <Foundation/Foundation.h>
-
-//---- @interface section ----
-
-    @interface Fraction: NSObject
-        -(void) print;
-        -(void) setNumerator: (int) n;
-        -(void) setDenominator: (int) d;
-        -(int) getNumerator;
-        -(int) getDenominator;
-    @end
-
-//---- @implementation section ----
-
-    @implementation Fraction {
-        int numerator;
-        int denominator;
-    }
-        -(void) print {
-            NSLog (@"%i/%i", numerator, denominator);
-        }
-
-        -(void) setNumerator: (int) n {
-            numerator = n;
-        }
-        -(void) setDenominator: (int) d {
-            denominator = d;
-        }
-
-        -(int) getNumerator {
-            return numerator;
-        }
-        -(int) getDenominator {
-            return denominator;
-        }
-
-    @end
-
-//---- program section ----
-
+#import "Fraction.h"
 int main (int argc, char * argv[]) {
     @autoreleasepool {
-       Fraction *myFraction = [[Fraction alloc] init];
-       // Fraction *myFraction = [Fraction new];
-       [myFraction setNumerator: 1];
-       [myFraction setDenominator: 3];
-
-       NSLog (@"The value of myFraction is: %i/%i",
-            [myFraction getNumerator], [myFraction getDenominator]);
+        Fraction *myFraction = [[Fraction alloc] init];
+        // set fraction to 1/3
+        [myFraction setNumerator: 1];
+        [myFraction setDenominator: 3];
+        // display the fraction
+        NSLog (@"The value of myFraction is:");
+        [myFraction print];
     }
-
 return 0;
 }
